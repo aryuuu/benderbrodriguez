1f4934251ea5f8d8f5f7b19a0299d9f89615aa7d
aryuuu <31617773+aryuuu@users.noreply.github.com>
2018-10-24 23:25:50 +0700

    Update mock.php

--- handler/mock.php
+++ handler/mock.php
@@ -2,7 +2,7 @@
 	use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 	function mock($name){
-		$result = new TextMessageBuilder("Hey ",$name,"! yo momma so fat she doesn't need the internet, because she's already world wide");
+		$result = new TextMessageBuilder("Hey " . $name . "! yo momma so fat she doesn't need the internet, because she's already world wide");
 
 		return $result;
 	}
