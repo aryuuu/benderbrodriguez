6989b0fa99becefc45c37f8b2bd36b1b15215fd6
fattah <herppratama@gmail.com>
2018-10-24 21:59:29 +0700

    new feature-calc

---
+++ handler/mathjs.php
@@ -0,0 +1,20 @@
+<?php 
+	use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder'
+
+	function calc($query){
+
+		$query = urlencode($query);
+		$result = file_get_contents('http://api_mathjs.org/v4/?expr='.$query);
+		$result = new TextMessageBuilder($result);
+
+		return $result;
+	}
+
+
+
+
+
+
+
+
+ ?>
