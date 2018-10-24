f2edf988a182f9deb7e34976030145f1e28a7a7b
fattah <herppratama@gmail.com>
2018-10-24 23:08:31 +0700

    uwaaah

--- handler/xkcd.php
+++ handler/xkcd.php
@@ -4,19 +4,19 @@
 
 	function xkcd($comicNumber)	{
 		if($comicNumber == null){
-			$json = file_get_contents('http://xkcd.com/info.0.json');
+			$json = file_get_contents("http://xkcd.com/info.0.json");
 			$json = json_decode($json, true);
 
 			$comicNumber = rand(1, $json['num']);
 		}
 
-		$json = file_get_contents('http://xkcd.com/'. $comicNumber . '/info.0.json' );
+		$json = file_get_contents("http://xkcd.com/". $comicNumber . "/info.0.json" );
 		$json = json_decode($json, true);
 
 		if (isset($json['img'])){
 			$result = new ImageMessageBuilder($json['img'], $json['img']);
 		} else {
-			$result = new TextMessageBuilder('xkcd ke-' . $comicNumber . 'tidak ada');
+			$result = new TextMessageBuilder("xkcd ke-" . $comicNumber . "tidak ada");
 		}
 
 		return result;
