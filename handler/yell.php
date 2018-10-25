8969c5ea32f2ac9e93ed71b688ce18e603932631
fattah <herppratama@gmail.com>
2018-10-25 07:48:48 +0700

    upgraded yell

--- handler/yell.php
+++ handler/yell.php
@@ -1,12 +1,14 @@
 <?php  
 	use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+	use \LINE\LINEBot\MessageBuilder\AudioMessageBuilder as AudioMessageBuilder;
 
 	function yell(){
-		$result = new TextMessageBuilder("Bite my shiny metal ass");
+		$result1 = new TextMessageBuilder("Bite my shiny metal ass");
+		$result2 = new AudioMessageBuilder("sound/yell.mp3");
 		
 
 
-		return $result;
+		return array($result1,$result2);
 	}
 
 
